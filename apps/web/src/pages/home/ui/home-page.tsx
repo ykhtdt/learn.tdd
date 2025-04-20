05abbe081ec3e977cba350147477210340ca5858
jgpark-1 <jgpark7281@gmail.com>
2025-04-21 00:37:37 +0900

    feat(apps/web): todo 완료 상태 변경 및 제거 기능 적용

--- apps/web/src/pages/home/ui/home-page.tsx
+++ apps/web/src/pages/home/ui/home-page.tsx
@@ -4,13 +4,22 @@ import type { Todo } from "@/entities/todo"
 
 import { useState } from "react"
 
-import { PlusIcon } from "lucide-react"
+import {
+  PlusIcon,
+  TrashIcon,
+} from "lucide-react"
 
+import { cn } from "@workspace/ui/lib/utils"
 import { Input } from "@workspace/ui/components/input"
 import { Button } from "@workspace/ui/components/button"
+import { Checkbox } from "@workspace/ui/components/checkbox"
 
 import { BaseLayout } from "@/app/layouts"
-import { createTodo } from "@/entities/todo"
+import {
+  createTodo,
+  deleteTodo,
+  toggleTodo,
+} from "@/entities/todo"
 
 export const HomePage = () => {
   const [todos, setTodos] = useState<Todo[]>([])
@@ -31,9 +40,36 @@ export const HomePage = () => {
     setTodos([...todos, newTodo])
   }
 
+  const toggleComplete = (id: string) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return toggleTodo(todo)
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  const handleDeleteTodo = (id: string) => {
+    const targetTodo = todos.find((todo) => todo.id === id)
+
+    if (targetTodo) {
+      const updatedTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return deleteTodo(todo)
+        }
+        return todo
+      })
+
+      setTodos(updatedTodos)
+    }
+
+  }
+
   return (
     <BaseLayout>
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8 mb-10">
         <h1 className="text-base font-bold tracking-wider">
           Todo List
         </h1>
@@ -47,6 +83,27 @@ export const HomePage = () => {
             <PlusIcon />
           </Button>
         </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        {todos.filter((todo) => !todo.deletedAt).map((todo) => (
+          <div key={todo.id} className="flex items-center justify-between gap-2 px-4 py-2 rounded-md border">
+            <div className="flex items-center gap-2">
+              <Checkbox id={todo.id} checked={todo.completed} onCheckedChange={() => toggleComplete(todo.id)} />
+              <label
+                htmlFor={todo.id}
+                className={cn("cursor-pointer text-sm", {
+                  "line-through": todo.completed,
+                })}
+              >
+                {todo.title}
+              </label>
+            </div>
+            <Button variant="destructive" size="icon" onClick={() => handleDeleteTodo(todo.id)} className="cursor-pointer">
+              <TrashIcon />
+            </Button>
+          </div>
+        ))}
       </div>
     </BaseLayout>
   )
