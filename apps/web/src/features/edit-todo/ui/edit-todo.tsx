9d6ba5731ef38cb8b2a42d25157730ae6445093c
Jonggwang Park <jgpark7281@gmail.com>
2025-04-21 18:00:03 +0900

    feat(apps/web): add edit-todo

---
+++ apps/web/src/features/edit-todo/ui/edit-todo.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import {
+  useState,
+  useCallback,
+} from "react"
+
+import {
+  CheckIcon,
+  XIcon,
+} from "lucide-react"
+
+import { Input } from "@workspace/ui/components/input"
+import { Button } from "@workspace/ui/components/button"
+
+interface EditTodoProps {
+  defaultValue: string
+  onChange: (title: string) => void
+  onSave: (title: string) => void
+  onReset: () => void
+}
+
+export const EditTodo = ({
+  defaultValue,
+  onChange,
+  onSave,
+  onReset,
+}: EditTodoProps) => {
+  const [title, setTitle] = useState(defaultValue)
+
+  const handleSave = useCallback(() => {
+    onSave(title)
+  }, [onSave, title])
+
+  const handleTitleChange = useCallback((event: React.FormEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value)
+    onChange(event.currentTarget.value)
+  }, [onChange])
+
+  const handleEnterKeyPress = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onSave(title)
+    }
+  }, [onSave, title])
+
+  return (
+    <div className="flex items-center gap-2">
+      <Input
+        value={title}
+        onInput={handleTitleChange}
+        onKeyDown={handleEnterKeyPress}
+        autoFocus
+      />
+      <Button onClick={handleSave} className="cursor-pointer">
+        <CheckIcon />
+      </Button>
+      <Button onClick={onReset} className="cursor-pointer">
+        <XIcon />
+      </Button>
+    </div>
+  )
+}
